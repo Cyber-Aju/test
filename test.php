ad0dbe30a23b89d17e9925ec9f0a53245cdd1191
Cyber-Aju <ajmalakram152@gmail.com>
2024-11-15 10:30:13 +0530

    test copied

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+function getProjects()
+{
+    $redmine = service('redmine');
+    $client = $redmine->getClient(session('redmine_api_key'));
+    $users = $client->getApi('issue')->all();
+    // $users = $client->getApi('project')->show('5');
+    // (['limit' => 50, 'offset' => 50]);
+    // print_r($users);
+    // $projects = [
+    //     'cms', 'cbt', 'b2b'
+    // ];
+    return json_encode($users);
+}
+
+?>
